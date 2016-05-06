a1b4c8a413af4b61386df9fe821b46387c75bf27
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-05-06 01:46:51 +0100

    added imagemagick

--- Core/OCR.fs
+++ Core/OCR.fs
@@ -4,6 +4,8 @@ open System
 open System.IO
 open Tesseract
 open System.Drawing
+open ImageMagick
+open FredsImageMagickScripts
 
 module OCR =
     
@@ -11,7 +13,19 @@ module OCR =
     type Confidence = Confidence of float32
 
     let getText imgStream =
+        
+        // Use ImageMagick to process image before OCR
+        // 
+        use img = new MagickImage(new Bitmap(Bitmap.FromStream imgStream))
+        let cleaner = TextCleanerScript()
+        cleaner.FilterOffset <- new Percentage(9.1)
+        cleaner.Trim <- true
+
+        // Use processed image with Tesseract OCR to get text
+        //
         use engine = new TesseractEngine("tessdata", "eng")
-        use img    = PixConverter.ToPix (new Bitmap(Bitmap.FromStream imgStream))
+        use img    = PixConverter.ToPix (cleaner.Execute(img).ToBitmap())
         use page   = engine.Process img
+
+        // Result
         TextResult (page.GetText()), Confidence (page.GetMeanConfidence())
